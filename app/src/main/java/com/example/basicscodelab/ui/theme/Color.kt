96a0c30b9a94da159c51e1f549bdb111f05b53ae
nicolashmesias <nikomesias3@gmail.com>
2024-07-31 23:05:28 -0500

    first commit

---
+++ app/src/main/java/com/example/basicscodelab/ui/theme/Color.kt
@@ -0,0 +1,8 @@
+package com.example.basicscodelab.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Blue = Color(0xFF2196F3)
+val LightBlue = Color(0xFFBBDEFB)
+val Navy = Color(0xFF0A0F3A)
+val Chartreuse = Color(0xFF7FFF00)
